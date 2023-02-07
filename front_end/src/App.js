34c0c92bc68e9779a0842d78f86e24d008515c4b
Tao BOURMAUD <tbourmaud@nextdecision.local>
2023-02-07 11:06:46 +0100

    homepage valid + route vers login

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -3,6 +3,7 @@ import './css/App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from './Layout/Layout.js'
 import HomePage from './components/Home/HomePage';
+import LoginPage from './components/Login/LoginPage';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
-            {/* <Route path="blogs" element={<Blogs />} /> */}
+            <Route path="login" element={<LoginPage />} />
             {/* <Route path="contact" element={<Contact />} /> */}
             {/* <Route path="*" element={<NoPage />} /> */}
           </Route>
