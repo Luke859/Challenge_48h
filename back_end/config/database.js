e539b334351229518d987d4a11072d978be11861
Luke859 <71589052+Luke859@users.noreply.github.com>
2023-02-08 00:10:38 +0100

    back final

--- back_end/config/database.js
+++ back_end/config/database.js
@@ -4,3 +4,8 @@ mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://localhost:27017/challenge_48h")
   .then(() => console.log('Connected to database ✅'))
   .catch((e) => console.log(`Cannot find database ❌ : ${e}`))
+
+
+
+
+  
