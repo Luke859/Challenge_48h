b947d2ce47b6c8701d7c43600397a654586413b5
baremy <remy.bamas@ynov.com>
2023-02-07 15:19:51 +0100

    set form and error password

--- front_end/src/components/Login/LoginPage.js
+++ front_end/src/components/Login/LoginPage.js
@@ -5,17 +5,12 @@ function LoginPage() {
       <>
       <div class="login">
         <h1>Bon retour parmis nous !</h1>
-      <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-      </div>
-      <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-          <input type="password" class="form-control" id="exampleFormControlInput2" placeholder="password"/>
-      </div>
-      <div>
-        <button type="submit" class="submit" id="exampleFormControlInput3">Se Connecter</button>
-      </div>
+        <form>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="email"/>
+            <input type="password" class="form-control" id="exampleFormControlInput2" placeholder="password"/>
+            <button type="submit" class="submit" id="exampleFormControlInput3">Se Connecter</button>
+        </form>
+      
       </div>
       </>
       );
--- front_end/src/components/SigninPage/SigininPage.js
+++ front_end/src/components/SigninPage/SigininPage.js
@@ -1,25 +1,36 @@
 import '../../css/Login.css'
+import React from "react"
+
 
 function SigninPage() {
+
+    const [password, setPassword] = React.useState('')
+    const [validatePassword, setValidatePassword] = React.useState('')
+    const [error, setError] = React.useState('')
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+
+        if (password != validatePassword) {
+            setError('Les mots de passe ne sont pas identiques')
+        }
+    };    
+
+
     return (
       <>
       <div class="login">
         <h1>Bienvenue parmis nous !</h1>
-      <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-      </div>
-      <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-          <input type="password" class="form-control" id="exampleFormControlInput2" placeholder="password"/>
-      </div>
-      <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-          <input type="password" class="form-control" id="exampleFormControlInput2" placeholder=" confirm password"/>
-      </div>
-      <div>
-        <button type="submit" class="submit" id="exampleFormControlInput3">S'inscrire</button>
-      </div>
+        <form class="form" onSubmit={handleLogin}>
+            <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="name"/>
+            <input type="surname" class="form-control" id="exampleFormControlInput2" placeholder="surname"/>
+            <input type="email" class="form-control" id="exampleFormControlInput3" placeholder="email"/>
+            <input type="password" class="form-control" id="exampleFormControlInput4" placeholder="password" onChange={e => setPassword(e.target.value)}/>
+            <input type="password" class="form-control" id="exampleFormControlInput5" placeholder=" confirm password" onChange={e => setValidatePassword(e.target.value)}/>
+            <button type="submit" class="submit" id="exampleFormControlInput6">S'inscrire</button>
+            {error && <span class="error">{error}</span>}
+        </form>
+        
       </div>
       </>
       );
